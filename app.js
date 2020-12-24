ecd0f9299a5b4ac390357f9d17ddd3ebdc3695d3
aves box <avesbox2020@gmail.com>
2020-12-24 23:18:24 +0700

    first commit

--- app.js
+++ app.js
@@ -3,11 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var debug = require('debug')('avesboxapi:server');
-var http = require('http');
-
-// PORT
-var port = process.env.PORT || '3000';
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,6 +12,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.set('port', process.env.PORT || '5000');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -43,6 +40,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(port, () => {
-  console.log("App running on port " + port)
+app.listen(app.get('port'), () => {
+  console.log("App running on port " + app.get('port'))
 });
